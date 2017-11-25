50cb91db7b8003732165117f91a8ba0715672e23
purkayasta <pritompurkayasta@gmail.com>
2017-11-26 02:22:17 +0600

    tweet me added!

--- Intermediate Front End Development Projects/Build a Random Quote Machine/JS/app.js
+++ Intermediate Front End Development Projects/Build a Random Quote Machine/JS/app.js
@@ -1,4 +1,5 @@
 
+
 function getRandom() {
     var urlCheck = 'https://api.forismatic.com/api/1.0/?';
     $.ajax({
@@ -14,9 +15,16 @@ function getRandom() {
             //console.log(response.quoteText);
             $('.card-text').html(response.quoteText);
             $('.card-title').text(response.quoteAuthor);
+            $("#tweetThis").attr("href", "https://twitter.com/intent/tweet?text="+response.quoteText+"--"+response.quoteAuthor+" ");
         }
     })  
 }
+
+
+$(function() {
+    getRandom();
+  });
+  
 
 $("#getNew").on("click", function() {
     getRandom();
